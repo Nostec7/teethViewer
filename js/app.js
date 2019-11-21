4c58388ce7e0e15652edfd92a4813144da750991
PauliusL <pauliusl181@gmail.com>
2019-11-21 17:36:17 +0200

    AdjustedLights

--- js/app.js
+++ js/app.js
@@ -47,10 +47,13 @@ function createLights() {
 
   const ambientLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 5 );
 
-  const mainLight = new THREE.DirectionalLight( 0xffffff, 5 );
-  mainLight.position.set( 10, 10, 10 );
+  const leftLight = new THREE.DirectionalLight( 0xffffff, 5 );
+  leftLight.position.set( 20, 10, 10 );
+	
+  const rightLight = new THREE.DirectionalLight( 0xffffff, 5 );
+  rightLight.position.set( -20, 10, 10 );	
 
-  scene.add( ambientLight, mainLight );
+  scene.add( ambientLight, leftLight, rightLight );
 
 }
 
