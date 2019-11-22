f1c4fc1f4184a3a2bc4c80f3c3c3fa3d28657709
PauliusL <pauliusl181@gmail.com>
2019-11-22 10:52:36 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ let globalLoader;
 let animationMaxTime = 1.99; //Animation time when mouth is fully opened
 let animationLength = animationMaxTime * 100;
 
+let raycaster, mouse = { x : 0, y : 0 };
+
 
 const mixers = [];
 const clock = new THREE.Clock();
@@ -29,6 +31,9 @@ function init() {
   loadModels();
   createRenderer();
   addMouthOpeningScrubber();
+	
+  raycaster = new THREE.Raycaster();
+  renderer.domElement.addEventListener( 'click', raycast, false );
 
   renderer.setAnimationLoop( () => {
 
@@ -43,13 +48,20 @@ function createCamera() {
 
   camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 1, 5000 );
   camera.position.set( 0, 1, 16 );
+  
+	
 
 }
 
 function createControls() {
 
   controls = new THREE.OrbitControls( camera, container );
-
+  controls.enableDamping = true;
+  controls.dampingFactor = 1.2;
+  controls.minPolarAngle = 0.0;
+  controls.maxPolarAngle = 2.9;
+  controls.update();
+	
 }
 
 function createLights() {
@@ -225,5 +237,62 @@ function seekAnimationTime(animMixer, timeInSeconds){
     animMixer.update(timeInSeconds)
   }
 }
+
+
+//======================
+//===== RAYCASTING =====
+//Step 1
+function raycast ( e ) {
+// Step 1: Detect light helper
+    //1. sets the mouse position with a coordinate system where the center
+    //   of the screen is the origin
+    mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+
+    //2. set the picking ray from the camera position and mouse coordinates
+    raycaster.setFromCamera( mouse, camera );    
+
+    //3. compute intersections (note the 2nd parameter)
+    var intersects = raycaster.intersectObjects( scene.children, true );
+
+    for ( var i = 0; i < intersects.length; i++ ) {
+        console.log( intersects[ i ] ); 
+        /*
+            An intersection has the following properties :
+                - object : intersected object (THREE.Mesh)
+                - distance : distance from camera to intersection (number)
+                - face : intersected face (THREE.Face3)
+                - faceIndex : intersected face index (number)
+                - point : intersection point (THREE.Vector3)
+                - uv : intersection point in the object's UV coordinates (THREE.Vector2)
+        */
+    }
+	// Step 2: Detect normal objects
+    //1. sets the mouse position with a coordinate system where the center
+    //   of the screen is the origin
+    mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
+
+    //2. set the picking ray from the camera position and mouse coordinates
+    raycaster.setFromCamera( mouse, camera );    
+
+    //3. compute intersections (no 2nd parameter true anymore)
+    var intersects = raycaster.intersectObjects( scene.children );
+
+    for ( var i = 0; i < intersects.length; i++ ) {
+        console.log( intersects[ i ] ); 
+        /*
+            An intersection has the following properties :
+                - object : intersected object (THREE.Mesh)
+                - distance : distance from camera to intersection (number)
+                - face : intersected face (THREE.Face3)
+                - faceIndex : intersected face index (number)
+                - point : intersection point (THREE.Vector3)
+                - uv : intersection point in the object's UV coordinates (THREE.Vector2)
+        */
+    }
+
+}
+
 
 init();
